146dca12dd5734ce2df05a62fe637780b1cad1bd
Mix1e <denisoffmixa@yandex.ru>
2023-08-13 16:08:44 +0400

    init commit lint fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {NgOptimizedImage} from "@angular/common";
-import {AppService} from "./app.service";
-import {HttpClientModule} from "@angular/common/http";
+import { NgOptimizedImage } from '@angular/common';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    NgOptimizedImage,
-  ],
-  providers: [AppService],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [HttpClientModule, BrowserModule, AppRoutingModule, NgbModule, NgOptimizedImage],
+    providers: [AppService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
